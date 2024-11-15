5fbafe72bded44911e37e91264f37a1a9b754f24
sharanggupta <sharang.gupta@sharanggupta.dev>
2024-11-15 20:49:46 +0000

    first commit

---
+++ src/main/java/dev/sharanggupta/gs_rest_service/GsRestServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.gs_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sharanggupta/gs_rest_service/GsRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.gs_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
